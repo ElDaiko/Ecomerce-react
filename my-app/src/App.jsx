5d4f5a9ebacd3c6d39058132fc3a2c64e2e5695e
Miguel Roldán <mirole16@gmail.com>
2022-08-18 12:14:03 -0500

    Deleting some items

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">EComerce</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
