f3872323cf7c85974f62cb41404383736a6ed1fb
Miguel Roldán <mirole16@gmail.com>
2022-08-22 20:08:03 -0500

    Feat: renaming folder

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,5 +1,5 @@
 import {React, useState} from 'react';
-import Navbar from './Navbar';
+import Navbar from './Nasvbar';
 import ItemListContainer from './ItemListContainer';
 
 //conectar mi html con el react
