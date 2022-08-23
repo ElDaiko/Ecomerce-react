7dc0354a7ce674ba6b937f32ed12bf53e9fbd518
Miguel Roldán <mirole16@gmail.com>
2022-08-22 19:47:48 -0500

    Feat: adding a buttom

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -4,10 +4,23 @@ import ItemListContainer from './ItemListContainer';
 
 //conectar mi html con el react
 function App() {
+
+  function saludar() {
+    console.log("Hola")
+  }
   return (
     <>
         <Navbar />
         <ItemListContainer title="Bendu clothing"/>
+        <p>
+          
+        </p>
+        <button onClick={saludar}>
+          Diste click
+        </button>
+        <button>
+
+        </button>
     </>
   );
 }
