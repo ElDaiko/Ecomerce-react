2f6101744c72ccbdeb132cd9d2b13a7486674711
Miguel Roldán <mirole16@gmail.com>
2022-08-28 12:43:02 -0500

    Feat: adding some basic css to the card container and an image

--- my-app/src/components/ItemCount.jsx
+++ my-app/src/components/ItemCount.jsx
@@ -1,30 +1,30 @@
-import {React} from 'react';
+import { React } from 'react';
 import UseCont from '../hooks/useCont';
 const ItemCount = ({ stock, onAdd, onReset }) => {
 
-    const {add, sub, cont} = UseCont(stock)
+    const { add, sub, cont } = UseCont(stock)
 
     return (
-
-        <div>
+        <div className='cardcontainer'>
+            <div className="card text-white bg-primary mb-3 btncont">
+            <div className="hoodie"></div>
             <div>
                 <p>Stock: {stock} </p>
+
+                <p>
+                    Cart: {cont}
+                </p>
                 <button className='btn' onClick={add}>
                     +
                 </button>
-                <p>
-                    cantidad: {cont}
-                </p>
                 <button className='btn' onClick={sub}>
                     -
                 </button>
-                <button className='btn' onClick={() => onReset()}>Reset</button>
             </div>
-            <div>
-                <button className="btn" onClick={() => onAdd(cont)}>Confirmar</button>
-            </div>
+            <button className='btn' onClick={() => onReset()}>Reset</button>
+            <button className="btn" onClick={() => onAdd(cont)}>Confirm</button>
         </div>
-
+        </div>
     );
 
 }
