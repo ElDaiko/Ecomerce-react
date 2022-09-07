7bac3ad7a21c8fb257f2927b8d1d79f0d77a6e9d
Miguel Roldán <mirole16@gmail.com>
2022-09-07 16:36:22 -0500

    Fix: destructuring the itemcount component and changing the item render

--- my-app/src/components/ItemContainer/Item.jsx
+++ my-app/src/components/ItemContainer/Item.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ItemCount from './ItemCount';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Item = ({ producto }) => {
 
+    const [stock, setStock] = useState(producto.stock)
+
     return (
-            <div key={producto.id}>
-                <ItemCount producto={producto}/>
+        <div key={producto.id}>
+            <div className='cardcontainer'>
+                <div className="card text-white bg-primary mb-3 btncont">
+                    <div className="hoodie"></div>
+                    <div>
+                        <p>Stock: {stock} </p>
+                    </div>
+                    <ItemCount producto={producto} setStock={setStock} stock={stock}/>
+                    <Link className='btn' to={`item/${producto.id}`}>
+                        Details
+                    </Link>
+                </div>
             </div>
+        </div>
     );
 
 }
 
 export default Item;
+
+
--- my-app/src/components/ItemContainer/ItemCount.jsx
+++ my-app/src/components/ItemContainer/ItemCount.jsx
@@ -1,15 +1,10 @@
 import { React } from 'react';
 import UseCont from '../../hooks/useCont';
 import { FaRedo, FaCheckCircle } from 'react-icons/fa';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
 
+const ItemCount = ({ producto, setStock, stock }) => {
 
-const ItemCount = ({producto}) => {
-
-
-    const [stock, setStock] = useState(producto.stock)
-
+    
     function onAdd(cont) {
         if (stock > 0 && cont <= stock) {
             console.log("se han confirmado", cont, "productos")
@@ -26,21 +21,14 @@ const ItemCount = ({producto}) => {
     const { add, sub, cont } = UseCont(stock)
 
     return (
-        <div className='cardcontainer'>
-            <div className="card text-white bg-primary mb-3 btncont">
-                <div className="hoodie"></div>
-                <div>
-                    <p>Stock: {stock} </p>
-                    <button className='btn margin' onClick={sub}>-</button>
-                    {cont}
-                    <button className='btn margin' onClick={add}>+</button>
-                </div>
-                <button className="btn" onClick={() => onAdd(cont)}><FaCheckCircle /></button>
-                <button className='btn' onClick={() => onReset()}><FaRedo /></button>
-                <Link className='btn' to={`item/${producto.id}`}>
-                    Details
-                </Link>
+        <div>
+            <div>
+                <button className='btn margin' onClick={sub}>-</button>
+                {cont}
+                <button className='btn margin' onClick={add}>+</button>
             </div>
+            <button className="btn" onClick={() => onAdd(cont)}><FaCheckCircle /></button>
+            <button className='btn' onClick={() => onReset()}><FaRedo /></button>
         </div>
     );
 
